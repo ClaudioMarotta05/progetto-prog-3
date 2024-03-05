3bc8c50c4c43ed6d64920b00dc15d8bb8ebc2572
MarcoMichelePisacane <marcomichele.pisacane001@studenti.uniparthenope.it>
2024-03-05 09:25:41 +0100

    fine commenti

--- nomi_cose_città/src/App.java
+++ nomi_cose_città/src/App.java
@@ -2,7 +2,7 @@ import MVC.AppController;
 import MVC.model1;
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos; // Import necessario per utilizzare Pos
+import javafx.geometry.Pos; 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
